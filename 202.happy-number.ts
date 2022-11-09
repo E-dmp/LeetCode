44cbe7776aaec96f8ce15dcb21ba44944706ba01
E-dmp <ridemdem1510@gmail.com>
2022-11-10 01:34:34 +0900

    202.happy-number.ts

---
+++ 202.happy-number.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=202 lang=typescript
+ *
+ * [202] Happy Number
+ */
+
+// @lc code=start
+function isHappy(n: number): boolean {
+    if (n === 0) return false;
+
+    while (n != 1 && n != 4) {
+
+        let sumOfNumber: number = 0
+        let dividedNum: number = n
+
+        // 割り続けたらdividedNumは<0
+        while (dividedNum > 0) {
+
+            sumOfNumber += (dividedNum % 10) * (dividedNum % 10);
+            dividedNum = Math.floor(dividedNum / 10)
+
+        }
+
+        n = sumOfNumber
+    }
+
+    return n == 1
+};
+// @lc code=end
+
